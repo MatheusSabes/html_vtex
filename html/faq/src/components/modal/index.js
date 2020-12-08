4240888bec652d170d5a90ec0628f3a41f62e3c1
Matheus Sabes <admin@iMac-de-admin.local>
2020-12-08 17:13:22 -0300

    pagina faq

--- html/faq/src/components/modal/index.js
+++ html/faq/src/components/modal/index.js
@@ -2,6 +2,10 @@ import './style';
 
 
 
+jQuery( ".help__title" ).click(function() {
+    jQuery(this).parent().toggleClass("help__item--open");
+});
+
 jQuery( ".help__menu--item" ).click(function() {
     jQuery(this).toggleClass("help__menu--open");
 });
